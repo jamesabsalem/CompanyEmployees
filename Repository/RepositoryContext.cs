b6cdaa2904ea91331fdef4243a8024cb4eac75e9
James Absalem Lintu <jamesabsalem@gmail.com>
2023-11-29 01:01:58 +0600

    working with db context.

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
