d512c9c0cf16bf13e29b9bc5aff8c513dd34fbde
James Absalem Lintu <jamesabsalem@gmail.com>
2023-12-02 21:34:05 +0600

    migration done

--- CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
+++ CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
@@ -4,18 +4,24 @@ using Repository;
 
 namespace CompanyEmployees.ContextFactory
 {
-    public class RepositoryContextFactory: IDesignTimeDbContextFactory<RepositoryContext>
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
     {
-       public RepositoryContext CreateDbContext(string[] args)
+        public RepositoryContext CreateDbContext(string[] args)
         {
             var configuration = new ConfigurationBuilder()
-           .SetBasePath(Directory.GetCurrentDirectory())
-           .AddJsonFile("appsettings.json")
-           .Build();
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            // Log or debug to check the configuration values
+            var connectionString = configuration.GetConnectionString("sqlConnection");
+            Console.WriteLine($"ConnectionString: {connectionString}");
 
             var builder = new DbContextOptionsBuilder<RepositoryContext>()
-                .UseSqlServer(configuration.GetConnectionString("sqlConnectin"));
+            .UseSqlServer(connectionString, b => b.MigrationsAssembly("CompanyEmployees"));
+
             return new RepositoryContext(builder.Options);
+
         }
     }
 }
