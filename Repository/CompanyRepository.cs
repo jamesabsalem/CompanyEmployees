574dc0b0f34cefa2064b795b1bb788adba0ecec8
James Absalem Lintu <jamesabsalem@gmail.com>
2023-12-05 00:47:30 +0600

    working with repository

---
+++ Repository/CompanyRepository.cs
@@ -0,0 +1,13 @@
+﻿using Contracts;
+using Entities;
+
+namespace Repository
+{
+    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+    {
+        public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+
+        }
+    }
+}
---
+++ Repository/EmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using Contracts;
+using Entities;
+
+namespace Repository
+{
+    public class EmployeeRepository: RepositoryBase<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(RepositoryContext repositoryContext): base(repositoryContext)
+        {
+            
+        }
+    }
+}
