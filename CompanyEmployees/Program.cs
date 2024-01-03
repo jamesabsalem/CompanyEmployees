c6256a5dec15919d1f5d2d684d5b5d6d1b243005
James Absalem Lintu <jamesabsalem@gmail.com>
2024-01-04 01:05:20 +0600

    working with extension

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -29,6 +29,7 @@ builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
 
 builder.Services.AddControllers();
 
